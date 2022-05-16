d6d0cf7e99a2892369ced1a5cd31f86a4d4da388
askMartyn <martyn@automationlogic.com>
2022-05-16 13:14:54 +0100

    initial commit of web app

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, send_from_directory, jsonify, render_template
+import json
+import os
+import requests
+STYLE_SHEET_NUMBER='16'
+USERS_ENDPOINT="http://localhost:5000/users"
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return  render_template('base.html', 
+            style_sheet_number=STYLE_SHEET_NUMBER)
+
+@app.route("/users")
+def users():
+    with open('users.json') as users:
+        text = json.loads(users.read())
+    return jsonify(text)
+
+@app.route("/comment")
+def comment():
+    username = request.args.get('username')
+    comment  = request.args.get('comment')
+    with open('users.json') as db:
+       user_comments = json.loads(db.read())
+    if len(user_comments) > 16:
+       user_comments.pop(0)
+    user_comments.append({
+       "username": str(username),
+       "comment": str(comment)
+    })
+    with open('users1.json','w') as db:
+       db.write(json.dumps(user_comments, indent=4))
+
+    os.rename('users1.json', 'users.json') 
+    return jsonify(user_comments)
+    
+
+
+@app.route('/get-files/<path:path>',methods = ['GET','POST'])
+def get_files(path):
+
+    """Download a file."""
+    try:
+        return send_from_directory('downloads', path, as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
